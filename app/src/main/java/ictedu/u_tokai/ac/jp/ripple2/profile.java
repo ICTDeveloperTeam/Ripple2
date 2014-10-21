f18fa29d0b6872da5406d96878e167bd76177b97
Kamiya <1bdi1208@gmail.com>
2014-10-21 10:09:38 +0900

    Initial commit

---
+++ app/src/main/java/ictedu/u_tokai/ac/jp/ripple2/profile.java
@@ -0,0 +1,54 @@
+package ictedu.u_tokai.ac.jp.ripple2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class profile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    //ボタン遷移について
+    public void other(View v){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void good(View v){
+        Intent intent = new Intent(this,good.class);
+        startActivity(intent);
+    }
+
+    public void profile(View v){
+        Intent intent = new Intent(this,profile.class);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
